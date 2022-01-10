29fb4752e2e67439ca3b41b3dfcc8dc3dd1b173d
jachs <799516423@qq.com>
2022-01-10 13:50:17 +0800

    'QWER'

---
+++ Chess/src/main/java/com/jachs/chess/ChessMain.java
@@ -0,0 +1,12 @@
+package com.jachs.chess;
+
+/***
+ * @author zhanchaohan
+ *
+ */
+public class ChessMain{
+	public static void main(String args[]){
+		new MainJFrame("中国象棋：观棋不语真君子，棋死无悔大丈夫");
+	}
+}
+
